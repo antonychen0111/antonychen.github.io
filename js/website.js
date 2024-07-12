2a17cbf0de2abad3717478f1f1fa21883f50c710
陳盈誌 Antony Chen <87083193+antonychen0111@users.noreply.github.com>
2024-07-13 05:58:55 +0800

    Add files via upload

--- js/website.js
+++ js/website.js
@@ -14,16 +14,18 @@
 	}
 
 // 載入網站進度條
-window.onload = setTimeout( function(){
-	window.document.documentElement.scrollTop = 0;
-	document.body.style.overflow = 'hidden';
-}, 1)
+window.onload = function() {
+    setTimeout( function() {
+        window.document.documentElement.scrollTop = 0;
+        document.body.style.overflow = 'hidden';
+    }, 1);
 
-window.onload = setTimeout( function(){
-	document.body.style.overflow = 'visible';
-	document.querySelector("html").style.overflowY= 'scroll';
-	document.getElementById("loadingBar").style.display = 'none';
-}, 1000)
+    setTimeout( function() {
+        document.body.style.overflow = 'visible';
+        document.querySelector("html").style.overflowY = 'scroll';
+        document.getElementById("loadingBar").style.display = 'none';
+    }, 1000);
+}
 
 // 首頁圖前後按鈕
 function indexImagesNext(){
@@ -43,7 +45,7 @@ setInterval( function indexImagesChange(){
 		document.getElementById("indexImagesContainerContainer1").style.display = 'none';
 		document.getElementById("indexImagesContainerContainer2").style.display = 'flex';
 	}
-}, 100);
+}, 500);
 
 // 菜單按鈕
 setInterval( function iconMenuOpen(){
@@ -56,7 +58,7 @@ setInterval( function iconMenuOpen(){
 	}else {
 		document.getElementById("iconMenuOpen").style.display = 'none';
 	}
-}, 100);
+}, 500);
 
 // 回到頂端按鈕
 setInterval( function iconToTop(){
@@ -67,7 +69,7 @@ setInterval( function iconToTop(){
 	}else {
 		document.getElementById("iconUpward").style.display = 'none';
 	}
-}, 100);
+}, 500);
 
 // 回到頂端高度
 function toTop(){
