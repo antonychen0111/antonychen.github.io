782bedfb16754f155a45c4fc5dde4089993717f3
陳盈誌 Antony Chen <87083193+antonychen0111@users.noreply.github.com>
2024-09-09 06:26:25 +0800

    Add files via upload

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -77,34 +77,170 @@ function pagesBox01(){
 
 function pagesBox02(){
 	pages = 2;
-	document.getElementById("pagesBox").style.display = "none"; // 彈窗關閉
-	document.getElementById("iconPortfolioClosePages").style.display = "none"; // 彈窗按鈕隱藏
-	document.getElementById("portfolioIconContainerContainer").style.display = "flex"; // 按鈕顯示
-	document.querySelector("body").classList.remove("no-scroll"); // 允許滾動
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
 }
 
 // 頁面彈窗跳轉手機版
 
 function pagesBoxMobile01(){
-	document.getElementById("pagesBox").style.display = "none"; // 彈窗關閉
-	document.getElementById("iconPortfolioClosePages").style.display = "none"; // 彈窗按鈕隱藏
-	document.getElementById("portfolioIconContainerContainer").style.display = "flex"; // 按鈕顯示
-	document.querySelector("body").classList.remove("no-scroll"); // 允許滾動
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
 	document.getElementById("pages01").scrollIntoView({behavior: "smooth", block: "center"});
 }
 
 function pagesBoxMobile02(){
-	document.getElementById("pagesBox").style.display = "none"; // 彈窗關閉
-	document.getElementById("iconPortfolioClosePages").style.display = "none"; // 彈窗按鈕隱藏
-	document.getElementById("portfolioIconContainerContainer").style.display = "flex"; // 按鈕顯示
-	document.querySelector("body").classList.remove("no-scroll"); // 允許滾動
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
 	document.getElementById("pages02").scrollIntoView({behavior: "smooth", block: "center"});
 }
 
 function pagesBoxMobile03(){
-	document.getElementById("pagesBox").style.display = "none"; // 彈窗關閉
-	document.getElementById("iconPortfolioClosePages").style.display = "none"; // 彈窗按鈕隱藏
-	document.getElementById("portfolioIconContainerContainer").style.display = "flex"; // 按鈕顯示
-	document.querySelector("body").classList.remove("no-scroll"); // 允許滾動
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
 	document.getElementById("pages03").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile04(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages04").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile05(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages05").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile06(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages06").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile07(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages07").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile08(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages08").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile09(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages09").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile10(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages10").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile11(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages11").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile12(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages12").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile13(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages13").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile14(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages14").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile15(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages15").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile16(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages16").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile17(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages17").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile18(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages18").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile19(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages19").scrollIntoView({behavior: "smooth", block: "center"});
+}
+
+function pagesBoxMobile20(){
+	document.getElementById("pagesBox").style.display = "none";
+	document.getElementById("iconPortfolioClosePages").style.display = "none";
+	document.getElementById("portfolioIconContainerContainer").style.display = "flex";
+	document.querySelector("body").classList.remove("no-scroll");
+	document.getElementById("pages20").scrollIntoView({behavior: "smooth", block: "center"});
 }
